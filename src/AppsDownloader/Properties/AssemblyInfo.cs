e64ea80815835b9fb15e1fca79664a706650d7b1
Si13n7 <admin@si13n7.com>
2018-09-19 17:24:50 +0200

    Update assembly version numbers.

--- src/AppsDownloader/Properties/AssemblyInfo.cs
+++ src/AppsDownloader/Properties/AssemblyInfo.cs
@@ -20,4 +20,4 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("a3418f24-e739-45c2-b31e-f50f4124e600")]
 
-[assembly: AssemblyVersion("18.9.17.0")]
+[assembly: AssemblyVersion("18.9.19.0")]
