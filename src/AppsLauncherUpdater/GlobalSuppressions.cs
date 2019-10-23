0ee93cae94c70056ecf7aed3e3a62a2397581603
Si13n7 <admin@si13n7.com>
2019-10-23 06:44:15 +0200

    Update code analysis suppression lists.

--- src/AppsLauncherUpdater/GlobalSuppressions.cs
+++ src/AppsLauncherUpdater/GlobalSuppressions.cs
@@ -1,3 +1,8 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("Code Quality", "IDE0067:Dispose objects before losing scope", Justification = "<Pending>", Scope = "member", Target = "~M:Updater.Windows.MainForm.MainForm_Shown(System.Object,System.EventArgs)")]
+[assembly: SuppressMessage("Code Quality", "IDE0067:Dispose objects before losing scope", Justification = "False Positive", Scope = "member", Target = "~M:Updater.Windows.MainForm.MainForm_Shown(System.Object,System.EventArgs)")]
+[assembly: SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "False Positive", Scope = "member", Target = "~M:Updater.Windows.MainForm.MainForm_Shown(System.Object,System.EventArgs)")]
+[assembly: SuppressMessage("Usage", "CA2208:Instantiate argument exceptions correctly", Justification = "This can not be avoided in this case", Scope = "member", Target = "~M:Updater.Libraries.Network.DownloadArchiver")]
+[assembly: SuppressMessage("Usage", "CA2208:Instantiate argument exceptions correctly", Justification = "This can not be avoided in this case", Scope = "member", Target = "~M:Updater.Windows.MainForm.SetChangeLog(System.String[])")]
+[assembly: SuppressMessage("Usage", "CA2208:Instantiate argument exceptions correctly", Justification = "This can not be avoided in this case", Scope = "member", Target = "~M:Updater.Windows.MainForm.SetUpdateInfo(System.Boolean,System.String[])")]
+[assembly: SuppressMessage("Usage", "CA2234:Pass system uri objects instead of strings", Justification = "Strings are internally converted anyway")]
