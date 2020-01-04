01e1cb94cb760acdf9e52b865958823cdb4d8671
Si13n7 <admin@si13n7.com>
2020-01-04 15:00:42 +0100

    Update assembly version numbers.

--- src/AppsLauncherUpdater/Properties/AssemblyInfo.cs
+++ src/AppsLauncherUpdater/Properties/AssemblyInfo.cs
@@ -15,6 +15,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("a0969572-f61a-453b-9043-e9d1fa8e8240")]
 
-[assembly: AssemblyVersion("20.1.3.0")]
+[assembly: AssemblyVersion("20.1.4.0")]
 
 [assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.MainAssembly)]
